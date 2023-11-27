bb023bbfd49543e297d8139400913badd8d16539
Matthew Mawdsley <matthew@mawdsley.me>
2023-11-27 19:10:02 +0000

    Swap alt and space

--- keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
@@ -41,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────┤ ├──────────────────────────────┤
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   // ╰───────────────────────────────┤ ├──────────────────────────────╯
-                                   KC_DEL, LOWER,   KC_LGUI,      KC_RCTL,  KC_SPC,
-                                           RAISE,   KC_ENT,       KC_RALT
+                                   KC_DEL, LOWER,   KC_LGUI,      KC_RCTL,  KC_RALT,
+                                           RAISE,   KC_ENT,       KC_SPC
   //                           ╰────────────────╯ ╰───────────╯
   ),
 

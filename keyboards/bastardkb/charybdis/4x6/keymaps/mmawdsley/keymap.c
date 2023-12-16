ea721d46bea44ed3ad3354466e6ae339dc5f326f
Matthew Mawdsley <matthew@mawdsley.me>
2023-12-16 10:48:53 +0000

    Swap back

--- keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
@@ -48,21 +48,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_RAISE] = LAYOUT(
   // ╭──────────────────────────────╮ ╭───────────────────────────────╮
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-  // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS,  KC_TRNS, KC_TRNS,
-  // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,
-  // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,    KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_TRNS, KC_TRNS,
-  // ╰──────────────────────────────┤ ├───────────────────────────────╯
-                                  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,
-                                           KC_TRNS, KC_TRNS,    KC_TRNS
-  //                           ╰───────────────╯ ╰───────────╯
-  ),
-
-  [LAYER_LOWER] = LAYOUT(
-  // ╭──────────────────────────────╮ ╭───────────────────────────────╮
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
@@ -72,6 +57,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, MOUSE2,  MOUSE3,  MOUSE1,  DRGSCRL,    KC_RABK,   KC_MINS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS,
   // ╰──────────────────────────────┤ ├───────────────────────────────╯
                                   KC_TRNS, KC_TRNS, KC_TRNS,    KC_RALT, KC_TRNS,
+                                           KC_TRNS, KC_TRNS,    KC_TRNS
+  //                           ╰───────────────╯ ╰───────────╯
+  ),
+
+  [LAYER_LOWER] = LAYOUT(
+  // ╭──────────────────────────────╮ ╭───────────────────────────────╮
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+  // ├──────────────────────────────┤ ├───────────────────────────────┤
+       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS,    KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS,  KC_TRNS, KC_TRNS,
+  // ├──────────────────────────────┤ ├───────────────────────────────┤
+       KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,
+  // ├──────────────────────────────┤ ├───────────────────────────────┤
+       KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,    KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_TRNS, KC_TRNS,
+  // ╰──────────────────────────────┤ ├───────────────────────────────╯
+                                  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,    KC_TRNS
   //                           ╰───────────────╯ ╰───────────╯
   ),

93eb2b1e22dd276078b1513c6771574af49904d9
Matthew Mawdsley <matthew@mawdsley.me>
2024-01-04 20:21:44 +0000

    Add left alt.  Moved delete to raise layer.

--- keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
@@ -46,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────┤ ├──────────────────────────────┤
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   // ╰───────────────────────────────┤ ├──────────────────────────────╯
-                                   KC_DEL, RAISE,   KC_LGUI,      KC_RCTL,  KC_RALT,
-                                           LOWER,   KC_ENT,       KC_SPC
+                                   KC_LALT, RAISE,   KC_LGUI,      KC_RCTL,  KC_RALT,
+                                            LOWER,   KC_ENT,       KC_SPC
   //                           ╰────────────────╯ ╰───────────╯
   ),
 
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────┤ ├──────────────────────────────┤
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   // ╰───────────────────────────────┤ ├──────────────────────────────╯
-                                   KC_DEL, RAISE,   KC_LGUI,      KC_RCTL,  KC_RALT,
-                                           LOWER,   KC_SPC,       KC_ENT
+                                   KC_LALT, RAISE,   KC_LGUI,      KC_RCTL,  KC_RALT,
+                                            LOWER,   KC_SPC,       KC_ENT
   //                           ╰────────────────╯ ╰───────────╯
   ),
 
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────╮ ╭───────────────────────────────╮
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
+       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,      KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
        KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_CAPS,    KC_PLUS,   KC_LPRN, KC_RPRN, KC_EQL,  KC_TRNS, KC_TRNS,
   // ├──────────────────────────────┤ ├───────────────────────────────┤

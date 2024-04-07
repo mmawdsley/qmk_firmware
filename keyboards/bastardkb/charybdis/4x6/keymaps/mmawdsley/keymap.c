b86b4272c8a8559ee72b91d1adb17d357ff9fe0d
Matthew Mawdsley <matthew@mawdsley.me>
2024-04-07 20:06:08 +0100

    Move lower to the right-hand side

--- keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
@@ -28,6 +28,7 @@ enum charybdis_keymap_layers {
 #define RAISE MO(LAYER_RAISE)
 #define DEBUG MO(LAYER_DEBUG)
 #define CTL_ESC MT(MOD_LCTL,KC_ESC)
+#define CTL_QUOT MT(MOD_RCTL,KC_QUOT)
 #define MOUSE1 KC_MS_BTN1
 #define MOUSE2 KC_MS_BTN2
 #define MOUSE3 KC_MS_BTN3
@@ -42,11 +43,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────┤ ├──────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   // ├───────────────────────────────┤ ├──────────────────────────────┤
-        CTL_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+        CTL_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, CTL_QUOT,
   // ├───────────────────────────────┤ ├──────────────────────────────┤
         KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   // ╰───────────────────────────────┤ ├──────────────────────────────╯
-                                   KC_LALT, RAISE,   KC_LGUI,      KC_RCTL,  KC_RALT,
+                                   KC_LALT, RAISE,   KC_LGUI,      KC_RALT,  LOWER,
                                             LOWER,   KC_ENT,       KC_SPC
   //                           ╰────────────────╯ ╰───────────╯
   ),

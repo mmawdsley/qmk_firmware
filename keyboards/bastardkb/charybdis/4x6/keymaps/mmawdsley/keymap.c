70b427ceb2e331fc3205c7f9cb60bb36a076e311
Matthew Mawdsley <matthew@mawdsley.me>
2024-06-18 11:41:56 +0100

    Add sysrq key to debug layer

--- keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
@@ -98,16 +98,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_DEBUG] = LAYOUT(
   // ╭──────────────────────────────╮ ╭───────────────────────────────╮
-       KC_TRNS, DF_BASE, DF_GAME, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+       KC_DEL,  DF_BASE, DF_GAME, KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_BSLS,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PSCR, KC_TRNS,
+       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_PSCR,  KC_P,    KC_BSPC,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS,  KC_TRNS, KC_TRNS,
+       CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
   // ╰──────────────────────────────┤ ├───────────────────────────────╯
-                                  KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,
-                                           KC_TRNS, KC_TRNS,    KC_TRNS
+                                  KC_LALT, KC_TRNS, KC_LGUI,    KC_RCTL, KC_RALT,
+                                           KC_TRNS, KC_ENT,     KC_SPC
   //                           ╰───────────────╯ ╰───────────╯
   ),
 };

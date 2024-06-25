90d370b5763857b5e3f73b66db399d657799adfd
Matthew Mawdsley <matthew@mawdsley.me>
2024-06-25 19:59:08 +0100

    Restore volume keys

--- keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────┤ ├───────────────────────────────┤
        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_PSCR,  KC_P,    KC_BSPC,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
-       CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_QUOT,
+       CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_MUTE, KC_VOLD, KC_VOLU, KC_L,     KC_SCLN, KC_QUOT,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
   // ╰──────────────────────────────┤ ├───────────────────────────────╯

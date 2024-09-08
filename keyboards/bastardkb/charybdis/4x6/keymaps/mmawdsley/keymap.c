6e0b2c3654d3e04ca4b26480c00618a469dcd57f
Matthew Mawdsley <matthew@mawdsley.me>
2024-09-08 15:53:45 +0100

    Swapped page up and page down

--- keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mmawdsley/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────┤ ├───────────────────────────────┤
        KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_TRNS,    KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS,
   // ├──────────────────────────────┤ ├───────────────────────────────┤
-       KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,    KC_HOME, KC_PGUP, KC_PGDN, KC_END,   KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_TRNS, KC_TRNS,
   // ╰──────────────────────────────┤ ├───────────────────────────────╯
                                   KC_TRNS, DEBUG,   KC_TRNS,    KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,    KC_TRNS

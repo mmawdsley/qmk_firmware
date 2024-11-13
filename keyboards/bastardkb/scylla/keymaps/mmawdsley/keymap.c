79a8e440227a2208c7cdca3b848d0fef0a66667f
Matthew Mawdsley <matthew@mawdsley.me>
2024-11-13 15:32:18 +0000

    Layout fixes
    * Swapped enter and space
    * Fixed debug mode shift
    * Moved caps lock from lowered layer to raised layer

--- keyboards/bastardkb/scylla/keymaps/mmawdsley/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/mmawdsley/keymap.c
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├─────────────────────────────┤ ├───────────────────────────────┤
           KC_LSFT, KC_Z,   KC_X,    KC_C,   KC_V,   KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
     // ├─────────────────────────────┤ ├───────────────────────────────┤
-                                   KC_LALT, LOWER, KC_LGUI,       KC_RGUI, RAISE, KC_RALT,
+                                   KC_LALT, LOWER,  KC_LGUI,      KC_RGUI, RAISE, KC_RALT,
     // ├─────────────────────────────┤ ├───────────────────────────────┤
-                                           KC_LCTL,  KC_ENT,      KC_SPC,  KC_RCTL
+                                           KC_LCTL, KC_SPC,       KC_ENT,  KC_RCTL
     // ╰─────────────────────────────┤ ├───────────────────────────────╯
     ),
     [LAYER_LOWER] = LAYOUT_split_4x6_5(
@@ -72,11 +72,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├────────────────────────────────┤ ├───────────────────────────────┤
           KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
     // ├────────────────────────────────┤ ├───────────────────────────────┤
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_CAPS,        KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL,  KC_TRNS, KC_TRNS,
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS,        KC_PLUS, KC_LPRN, KC_RPRN, KC_EQL,  KC_TRNS, KC_TRNS,
     // ├────────────────────────────────┤ ├───────────────────────────────┤
           KC_TRNS, KC_TRNS, MOUSE2,  MOUSE3,  MOUSE1,  KC_TRNS,        KC_RABK, KC_MINS, KC_TRNS, KC_TRNS, KC_BSLS, KC_TRNS,
     // ├────────────────────────────────┤ ├───────────────────────────────┤
-                                     KC_LALT, KC_TRNS, KC_LGUI,        KC_RGUI, RAISE, KC_RALT,
+                                     KC_LALT, KC_TRNS, KC_LGUI,        KC_RGUI, DEBUG, KC_RALT,
     // ├────────────────────────────────┤ ├───────────────────────────────┤
                                               KC_LCTL, KC_ENT,         KC_SPC,  KC_RCTL
     // ╰────────────────────────────────┤ ├───────────────────────────────╯
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ├────────────────────────────────┤ ├────────────────────────────────┤
           KC_TRNS, KC_F1,   KC_F2,    KC_F3,   KC_F4,  KC_TRNS,        KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_TRNS,  KC_TRNS,
     // ├────────────────────────────────┤ ├────────────────────────────────┤
-          KC_TRNS, KC_F5,   KC_F6,    KC_F7,   KC_F8,  KC_TRNS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
+          KC_TRNS, KC_F5,   KC_F6,    KC_F7,   KC_F8,  KC_CAPS,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
     // ├────────────────────────────────┤ ├────────────────────────────────┤
           KC_TRNS, KC_F9,   KC_F10,   KC_F11,  KC_F12, KC_TRNS,        KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS,
     // ├────────────────────────────────┤ ├────────────────────────────────┤
